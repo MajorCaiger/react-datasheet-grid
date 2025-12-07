6e52ed527f82e9a75ea52c6a5c9129866a11995b
Rob Caiger <major.caiger@gmail.com>
2025-12-07 21:24:42 +0000

    dist

---
+++ dist/components/DataSheetGrid.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { DataSheetGridProps, DataSheetGridRef } from '../types';
+export declare const DataSheetGrid: React.NamedExoticComponent<DataSheetGridProps<any> & React.RefAttributes<DataSheetGridRef>>;
+//# sourceMappingURL=DataSheetGrid.d.ts.map
---
+++ dist/components/StaticDataSheetGrid.d.ts
@@ -0,0 +1,4 @@
+import { DataSheetGridProps, DataSheetGridRef } from '../types';
+import React from 'react';
+export declare const StaticDataSheetGrid: React.ForwardRefExoticComponent<DataSheetGridProps<any> & React.RefAttributes<DataSheetGridRef>>;
+//# sourceMappingURL=StaticDataSheetGrid.d.ts.map
